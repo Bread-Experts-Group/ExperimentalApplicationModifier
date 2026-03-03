41db06c0f9b73a7ee544fd2d07ef7ab17d3b161f
ttttdoy <logan3mclean@gmail.com>
2026-03-02 22:37:11 -0500

    update bsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ idea {
 dependencies {
 	testImplementation(kotlin("test"))
 	implementation(kotlin("reflect"))
-	implementation("org.bread_experts_group:bread_server_lib-code:D1F5N7P4")
+	implementation("org.bread_experts_group:bread_server_lib-code:D1F5N8P6")
 }
 
 kotlin {
